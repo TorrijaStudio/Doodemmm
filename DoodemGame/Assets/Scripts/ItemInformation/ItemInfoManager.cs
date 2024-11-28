e1b44f667ffb7f06c143bb8017244b14c51c42e5
Diego Martinez García <d.martinezg.2021@alumnos.urjc.es>
2024-11-28 22:20:03 +0100

    Info PopUps

--- DoodemGame/Assets/Scripts/ItemInformation/ItemInfoManager.cs
+++ DoodemGame/Assets/Scripts/ItemInformation/ItemInfoManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using tienda;
 using Unity.Mathematics;
 using UnityEngine;
@@ -11,7 +12,10 @@ namespace ItemInformation
 
         [SerializeField] private Transform center;
         [SerializeField] private float offset;
+        private GameObject _closeButtonSingle;
+        private GameObject _closeButtonMultiple;
         public static ItemInfoManager instance;
+        private List<GameObject> _openDisplays;
 
         private void Start()
         {
@@ -20,23 +24,47 @@ namespace ItemInformation
             {
                 instance = this;
             }
+
+            _closeButtonSingle = transform.Find("CloseButton1").gameObject;
+            _closeButtonSingle = transform.Find("CloseButton2").gameObject;
+            _closeButtonSingle.SetActive(false);
+            _closeButtonMultiple.SetActive(false);
+            _openDisplays = new List<GameObject>();
         }
 
         public void Display(ScriptableObjectTienda items)
         {
             if (items.objectsToSell.Count == 1)
             {
-                var display = Instantiate(displayToInstantiate);
+                var display = Instantiate(displayToInstantiate,center.position, quaternion.identity, transform);
                 display.DisplayItem(items);
+                _openDisplays.Add(display.gameObject);
+                _closeButtonSingle.SetActive(true);
             }
+            else {
+                var i = 1;
+                foreach (var it in items.objectsToSell)
+                {
+                    var display = Instantiate(displayToInstantiate, center.position + Vector3.up * offset * i,
+                        quaternion.identity, transform);
+                    display.DisplayItem(it.scriptableObjectTienda);
+                    _openDisplays.Add(display.gameObject);
+                    i--;
+                }
+                _closeButtonMultiple.SetActive(true);
+            }
+        }
 
-            var i = 1;
-            foreach (var it in items.objectsToSell)
+        public void CloseDisplays()
+        {
+            for (int i = _openDisplays.Count - 1; i >= 0; i--)
             {
-                var display = Instantiate(displayToInstantiate, center.position + Vector3.up*offset*i, quaternion.identity);
-                display.DisplayItem(it.scriptableObjectTienda);
-                i--;
+                Destroy(_openDisplays[i]);
             }
+            _openDisplays.Clear();
+            
+            _closeButtonSingle.SetActive(false);
+            _closeButtonMultiple.SetActive(false);
         }
     }
 }
