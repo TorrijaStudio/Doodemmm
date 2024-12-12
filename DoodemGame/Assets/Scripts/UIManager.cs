6fecb016a8e5f7ebfdb33d8751be2fdf26ff4f88
Mario Gonzalez <96237810+Brutus181@users.noreply.github.com>
2024-12-12 23:11:59 +0100

    COMMIT FINAL DOODEM
    Se acabo

--- DoodemGame/Assets/Scripts/UIManager.cs
+++ DoodemGame/Assets/Scripts/UIManager.cs
@@ -86,7 +86,7 @@ namespace HelloWorld
 
         void OnGUI()
         {
-                StatusLabels();
+                //StatusLabels();
         }
 //*************************************************************
 
